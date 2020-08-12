d65316089fb86b1f47d0b398dca7db658ea5b909
kaito.higa <kaito.higa.free@gmail.com>
2020-08-12 18:57:02 +0900

    divid handler

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,3 @@
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const ssr = require('./handler/ssr')
 
-const functions = require('firebase-functions')
-const { Nuxt } = require('nuxt')
-
-const nuxt = new Nuxt({
-  buildDir: 'ssr',
-  dev: false
-})
-
-exports.ssr = functions.https.onRequest(async (req, res) => {
-  await nuxt.ready()
-  return nuxt.render(req, res)
-})
+exports.ssr = ssr.hanler
