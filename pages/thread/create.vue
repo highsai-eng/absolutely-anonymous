76564154eca502324c6243e90902ab5021e36644
kaito.higa <kaito.higa.free@gmail.com>
2020-08-22 13:36:43 +0900

    add thread create

--- pages/thread/create.vue
+++ pages/thread/create.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>aa</div>
+  <div>
+    <v-row>
+      <h1>すれっどを作ってみる</h1>
+    </v-row>
+    <v-row>
+      <v-col cols="8">
+        <v-card class="mx-auto">
+          テストテスト
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        ffff
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
